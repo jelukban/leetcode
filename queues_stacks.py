4a2d8402ae56f91aafa597e43448d24a0f67b75d
Jerianne Lukban <jeriannelukban19@gmail.com>
2023-03-14 23:02:22 -0700

    Completed Implement Stack Using Queues

--- queues_stacks.py
+++ queues_stacks.py
@@ -43,3 +43,22 @@ def isValid(self, s: str) -> bool:
         return True
     else:
         return False
+
+
+# 225. Implement Stack using Queues
+class MyStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+
+    def pop(self) -> int:
+        return self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
