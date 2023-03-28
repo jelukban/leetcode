79f053ac14070bc074f3ec312662b1adc3123bb6
Jerianne Lukban <jeriannelukban19@gmail.com>
2023-03-27 18:49:13 -0700

    Completed 283. Move Zeroes

--- arrays.py
+++ arrays.py
@@ -5,3 +5,15 @@ def twoSum(nums, target):
 
         if value in nums[i+1:]:
             return [i, nums.index(value, i+1)]
+
+
+# 283. Move Zeroes
+def moveZeroes(nums):
+    num_of_zeroes = 0
+
+    while 0 in nums:
+        num_of_zeroes += 1
+        nums.remove(0)
+
+    for i in range(0, num_of_zeroes):
+        nums.append(0)
